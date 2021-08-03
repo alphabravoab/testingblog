8fbebc7c61c2f914e075ca427aef92e5710fce31
Arjen <9051543+alphabravoab@users.noreply.github.com>
2021-08-03 14:50:07 +0200

    posts working

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addCollection("posts", function(collection) {
+        return collection.getFilteredByGlob("posts/*.md");
+      });
 }
---
+++ posts/first.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/post.njk
+title: First post
+description: first test post
+date: 2021-08-02
+featuredImage: /images/uploads/image01.jpeg
+---
+
+This is the text content of this blog post.
